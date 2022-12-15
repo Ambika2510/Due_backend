90e7484a92c39b7459760acbe899e4e18d02a434
Ambika2510 <kumarambika2510@gmail.com>
2022-12-15 17:16:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const workoutRoutes = require('./routes/workout');
+
+//middleware
+app.use(cors());
+
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+//connect to db
+const port = 3003 || process.env.PORT
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.DBURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
+        app.listen(port, () => console.log(`connected to db and Server started on port ${port}`))
+    })
+    .catch((e) => console.log("hello"));
+//routes
+app.use('/api/workouts', workoutRoutes);
